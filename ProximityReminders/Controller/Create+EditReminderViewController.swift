d4b536171a42575d205c4cc83c48e3456746f274
andrewgraves(MAC) <andrewcgraves@gmail.com>
2019-12-30 14:06:31 -0800

    Got location and map working great!

--- ProximityReminders/Controller/Create+EditReminderViewController.swift
+++ ProximityReminders/Controller/Create+EditReminderViewController.swift
@@ -7,13 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class Create_EditReminderViewController: UIViewController {
     @IBOutlet weak var titleLabel: UINavigationItem!
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var addLocationButton: UIButton!
     @IBOutlet weak var approachingSwitcher: UISegmentedControl!
     @IBOutlet weak var singleUseSwitcher: UISegmentedControl!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // MARK: Helper Classes
+    lazy var locationManager: LocationManager = {
+        return LocationManager(locationDelegate: self, permissionsDelegate: nil)
+    }()
+    
     
     // MARK: Creation Variables
     var isEditingReminder = false
@@ -21,13 +29,39 @@ class Create_EditReminderViewController: UIViewController {
     var alertOnArrival: Bool = true
     var isRecurring: Bool = true
     
+    
     // MARK: Other variables
     var date: Date = Date()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupMap()
+    }
+    
+    // MARK: Helper functions
+    func createAlert(withTitle title: String, andDescription description: String){
+        let alertController = UIAlertController(title: title, message: description, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "dismiss", style: .cancel, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func setupMap() {
+        mapView.showsUserLocation = true
+    }
+    
+    func goToLocation(_ location: CLLocation) {
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func populateViewWithPlacemark(_ placemark: CLPlacemark) {
+        if let name = placemark.name, let locality = placemark.locality {
+            locationTextField.text = "\(name), \(locality)"
+
+        }
     }
         
     // MARK: Button Functions
@@ -51,10 +85,11 @@ class Create_EditReminderViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func addLocationPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+    @IBAction func getCurrentLocation(_ sender: Any) {
+        print("Get current location")
+        locationManager.requestLocation()
     }
-    
+   
     @IBAction func approachingSwitched(_ sender: Any) {
         switch approachingSwitcher.selectedSegmentIndex {
         case 0: alertOnArrival = true; print("alert on arrival")
@@ -70,6 +105,31 @@ class Create_EditReminderViewController: UIViewController {
         case 1: isRecurring = false; print("repeat")
         default:
             alertOnArrival = true
+        }
+    }
+}
+
+extension Create_EditReminderViewController: LocationManagerDelegate {
+    func obtainedLocation(_ location: CLLocation) {
+        print("obtained location!")
+        goToLocation(location)
+    }
+    
+    func obtainedPlacemark(_ placemark: CLPlacemark) {
+        print("obtained placemark")
+        populateViewWithPlacemark(placemark)
+    }
+    
+    func failedWithError(_ error: LocationError) {
+        switch error {
+        case .disallowedByUser:
+            createAlert(withTitle: "Location Access Needed", andDescription: "Please allow access to the location data to use this feature")
+
+        case .unableToFindLocation:
+            createAlert(withTitle: "Unable to Find Location", andDescription: "Somthing went wrong with the retrevial of your location...")
+            
+        case .unknownError:
+            createAlert(withTitle: "Unknown Error", andDescription: "There was an unknown error...")
         }
     }
 }
---
+++ ProximityReminders/Helpers/LocationManager.swift
@@ -0,0 +1,151 @@
+//
+//  LocationManager.swift
+//  ProximityReminders
+//
+//  Created by Andrew Graves on 12/30/19.
+//  Copyright © 2019 Andrew Graves. All rights reserved.
+//
+
+import CoreLocation
+import Network
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    weak var permissionsDelegate: LocationPermissionsDelegate?
+    weak var locationDelegate: LocationManagerDelegate?
+    private let monitor = NWPathMonitor()
+    
+    init(locationDelegate: LocationManagerDelegate?, permissionsDelegate: LocationPermissionsDelegate?) {
+        self.locationDelegate = locationDelegate
+        self.permissionsDelegate = permissionsDelegate
+        super.init()
+        
+        manager.delegate = self
+        monitor.pathUpdateHandler = { path in
+            if path.status == .satisfied {
+                print("Connection works correctly")
+                return
+            } else {
+                
+                DispatchQueue.main.async {
+                    locationDelegate?.failedWithError(LocationError.unableToFindLocation)
+                }
+            }
+        }
+    }
+    
+    
+    // MARK: Authorization Functions
+    static var isAuthorized: Bool {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse: return true
+        default: return false
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            permissionsDelegate?.authorizationSucceeded()
+        } else {
+            permissionsDelegate?.authorizationFailedWithStatus(status)
+        }
+    }
+    
+    func requestLocationAuthorization() throws {
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        
+        if authorizationStatus == .restricted || authorizationStatus == .denied {
+            throw LocationError.disallowedByUser
+        } else if authorizationStatus == .notDetermined {
+            manager.requestWhenInUseAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    // MARK: Location
+    func requestLocation() {
+        
+        // This checks to see if there is an active network connection
+        let queue = DispatchQueue(label: "Network Monitor")
+        monitor.start(queue: queue)
+        
+        do {
+            try requestLocationAuthorization()
+
+        } catch LocationError.disallowedByUser {
+            locationDelegate?.failedWithError(LocationError.disallowedByUser)
+        } catch LocationError.unableToFindLocation {
+            locationDelegate?.failedWithError(LocationError.unableToFindLocation)
+        } catch LocationError.unknownError {
+            locationDelegate?.failedWithError(LocationError.unknownError)
+        } catch {
+            fatalError("\(error)")
+        }
+        
+        manager.requestLocation()
+    }
+    
+    func getPlacemark(from location: CLLocation, completionHandler: @escaping (CLPlacemark?) -> Void) {
+        let geocoder = CLGeocoder()
+
+        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if error == nil {
+                let placemark = placemarks?[0]
+                completionHandler(placemark)
+
+            } else {
+                completionHandler(nil)
+            }
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        guard let error = error as? CLError else {
+            locationDelegate?.failedWithError(.unknownError)
+            return
+        }
+        
+        switch error.code {
+        case .locationUnknown: locationDelegate?.failedWithError(.unableToFindLocation)
+        case .network: locationDelegate?.failedWithError(.unableToFindLocation)
+        case .denied: locationDelegate?.failedWithError(.disallowedByUser)
+        default: return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            locationDelegate?.failedWithError(.unableToFindLocation)
+            return
+        }
+        
+        locationDelegate?.obtainedLocation(location)
+        
+        getPlacemark(from: location) { placemark in
+            guard let placemark = placemark else { return }
+            
+            self.locationDelegate?.obtainedPlacemark(placemark)
+
+        }
+    }
+}
+
+// MARK: Protocol and Enums
+enum LocationError: Error {
+    case unknownError
+    case disallowedByUser
+    case unableToFindLocation
+}
+
+protocol LocationPermissionsDelegate: class {
+    func authorizationSucceeded()
+    func authorizationFailedWithStatus(_ status: CLAuthorizationStatus)
+}
+
+protocol LocationManagerDelegate: class {
+    func obtainedLocation(_ location: CLLocation)
+    func obtainedPlacemark(_ placemark: CLPlacemark)
+    func failedWithError(_ error: LocationError)
+}
